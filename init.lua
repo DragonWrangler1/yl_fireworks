5da389087fcc21eaaa82507ff0630b6d9c91f78e
crispiebacon <joshua.b.newport@gmail.com>
2024-10-13 15:16:17 -0700

    fix globals

--- init.lua
+++ init.lua
@@ -26,12 +26,12 @@
 {"#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c","#8a7f6c"},
 	}
 
-register_firework_entity("yl_fireworks:yl_firework_entity", {
+fireworks_reimagined.register_firework_entity("yl_fireworks:yl_firework_entity", {
 	firework_shape = "yl",
 	firework_explosion = function(pos, shape)
-		register_firework_explosion(pos, 0.5, color_grid, 1)
+		fireworks_reimagined.register_firework_explosion(pos, 0.5, color_grid, 1)
 	end
 })
 
-register_firework_node("your_land", "yl_fireworks:yl_firework_entity", 0)
-register_firework_node("your_land", "yl_fireworks:yl_firework_entity", 10)
+fireworks_reimagined.register_firework_node("your_land", "yl_fireworks:yl_firework_entity", 0)
+fireworks_reimagined.register_firework_node("your_land", "yl_fireworks:yl_firework_entity", 10)
